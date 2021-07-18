fdf6739d5f2b58c2a71583d07b0200751e8adc1a
yuliia-panchenko <soprom321@gmail.com>
2021-07-18 17:49:01 +0300

    add hw_3-7

--- lesson_3-7/hw_3-7.py
+++ lesson_3-7/hw_3-7.py
@@ -7,47 +7,48 @@
 class Car:
     """Parameters of different cars"""
 
-    def __init__(self, model, year, manufacturer, engine, color, price):
+    def __init__(self, model, year, manufacturer, engine, colour, price):
         self.model = model
         self.year = year
         self.manufacturer = manufacturer
         self.engine = engine
-        self.color = color
+        self.colour = colour
         self.price = price
 
-    def car_info(self):
-        return f'Car model is {self.model}, ' \
-               f'manufactured by {self.manufacturer} in {self.year}, ' \
-               f'volume of engine is {self.engine}, ' \
-               f'color is {self.color}'
+    def get_car_model(self):
+        return f'Car model is {self.model}'
 
-    def car_price(self, design_price=0):
-        return f'Car price is {self.price + design_price}$'
+    def get_car_year(self):
+        return f'{self.model} was manufactured in {self.year}'
+
+    def get_car_manufacturer(self):
+        return f'{self.model} was manufactured in {self.manufacturer}'
+
+    def get_car_engine(self):
+        return f'The volume of {self.model} engine is {self.engine}'
+
+    def get_car_colour(self):
+        return f'The colour of {self.model} is {self.colour}'
+
+    def get_car_price(self):
+        return f'Car price is {self.price}$'
+
+    def set_car_price(self, modification_price=0):
+        return f'Car price with modification is {self.price + modification_price}$'
 
 
 mercedes = Car(
     model='Mercedes-Maybach S500', year=2015, manufacturer='Germany',
-    engine=4.0, color='red', price=90000
+    engine=4.0, colour='red', price=90000
 )
 
-porsche = Car(
-    model='Porsche Cayenne E-Hybrid', year=2020, manufacturer='Germany',
-    engine=3.0, color='green', price=145000
-)
-
-vaz = Car(
-    model='VAZ 2109', year=1992, manufacturer='Russia',
-    engine=1.5, color='black', price=1500
-)
-
-print(mercedes.car_info())
-print(mercedes.car_price(1000))
-print('========================')
-print(porsche.car_info())
-print(porsche.car_price())
-print('========================')
-print(vaz.car_info())
-print(vaz.car_price())
+print(mercedes.get_car_model())
+print(mercedes.get_car_year())
+print(mercedes.get_car_manufacturer())
+print(mercedes.get_car_engine())
+print(mercedes.get_car_colour())
+print(mercedes.get_car_price())
+print(mercedes.set_car_price(10000))
 print('========================')
 
 # Задание 2
@@ -59,7 +60,7 @@ print('========================')
 class Book:
     """Information of different books"""
 
-    def __init__(self, name, year, publisher, genre, author, cover, price):
+    def __init__(self, name, year, publisher, genre, author, price, cover='soft'):
         self.name = name
         self.year = year
         self.publisher = publisher
@@ -68,18 +69,18 @@ class Book:
         self.cover = cover  # переплет
         self.price = price
 
-    def book_info(self):
+    def get_book_info(self):
         return f'Book: {self.name} is a {self.genre} ' \
                f'written by {self.author}, ' \
-               f'published by {self.publisher} in {self.year} ' \
-               f'(in {self.cover} cover)'
+               f'published by {self.publisher} in {self.year} '
 
-    def book_price(self):
+    def get_book_price(self):
         hard_cover_price = 80  # цена за твердый переплет
         if self.cover == 'soft':
-            return f'Book price is {self.price} UAH'
+            return f'The price of the book in {self.cover} cover is {self.price} UAH'
         else:
-            return f'Book price is {self.price + hard_cover_price} UAH'
+            return f'The price of the book in {self.cover} cover is ' \
+                   f'{self.price + hard_cover_price} UAH'
 
 
 book1 = Book(
@@ -88,17 +89,18 @@ book1 = Book(
     cover='hard', price=500
 )
 book2 = Book(
-    name='The Kite Runner', year=2003,
-    publisher='	Riverhead Books', genre='historical fiction', author='Khaled Hosseini',
-    cover='soft', price=120
+    name='The Kite Runner', year=2003, publisher='Riverhead Books',
+    genre='historical fiction', author='Khaled Hosseini',
+    price=120
 )
 
-print(book1.book_info())
-print(book1.book_price())
+print(book1.get_book_info())
+print(book1.get_book_price())
 print('========================')
-print(book2.book_info())
-print(book2.book_price())
+print(book2.get_book_info())
+print(book2.get_book_price())
 print('========================')
+
 
 # Задание 3
 # Реализуйте класс «Стадион». Необходимо хранить в полях класса: название стадиона,
@@ -116,22 +118,25 @@ class Stadium:
         self.city = city
         self.capacity = capacity
 
-    def stadium_info(self):
-        return f'The name of stadium is {self.name} ' \
-               f'was opened in {self.opening_date} in {self.city}, ' \
-               f'{self.country}. The capacity of stadium is {self.capacity}.'
+    def get_stadium_name(self):
+        return f'The name of a stadium is {self.name}'
+
+    def get_stadium_opening_date(self):
+        return f'The stadium {self.name} was opened in {self.opening_date}'
+
+    def get_stadium_location(self):
+        return f'The stadium {self.name} is situated in {self.city}, {self.country}'
+
+    def get_stadium_capacity(self):
+        return f'The capacity of the stadium {self.name} is {self.capacity}'
 
 
 ukr_stadium = Stadium(
     name='The Olympic National Sports Complex', country='Ukraine',
     city='Kyiv', opening_date='12 September 1923', capacity=70050
 )
-eng_stadium = Stadium(
-    name='Wembley Stadium', opening_date=2007, city='London',
-    country='England', capacity=90000
-)
 
-print(ukr_stadium.stadium_info())
-print('========================')
-print(eng_stadium.stadium_info())
-print('========================')
+print(ukr_stadium.get_stadium_name())
+print(ukr_stadium.get_stadium_opening_date())
+print(ukr_stadium.get_stadium_location())
+print(ukr_stadium.get_stadium_capacity())
